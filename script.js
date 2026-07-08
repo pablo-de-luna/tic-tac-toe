57d1131d95a69a09784efc9f3d5df213520fbced
Pablo De Luna <205702351+pablo-de-luna@users.noreply.github.com>
2026-07-08 11:00:01 -0600

    Add function createPlayers

--- script.js
+++ script.js
@@ -2,13 +2,20 @@
 
 "use strict"
 
-const createPlayer = (name, mark) => {
-  return {name, mark};
+const createPlayers = (player1Name = "player1", player2Name = "player2") => {
+  return {
+    player1: {
+      name: player1Name,
+      mark: "x",
+    },
+    player2: {
+      name: player2Name,
+      mark: "o",
+    }
+  };
 };
 
 const gameboard = (() => {
-  const player1 = createPlayer("player1", "O");
-  const player2 = createPlayer("player2", "X");
   const board = [
     [0, 0, 0],
     [0, 0, 0],
@@ -17,8 +24,8 @@ const gameboard = (() => {
 
   const getBoard = () => board;
   
-  const addPlayerMark = (playerMark, rowNumber, columnNumber) => {
-    board[rowNumber][columnNumber] = playerMark.mark;
+  const addPlayerMark = (player, row, column) => {
+    board[row][column] = player.mark;
   }
 
   return {getBoard, addPlayerMark};
