e89eabe2be9617b1968b83ea0c0984488d8df60f
Pablo De Luna <205702351+pablo-de-luna@users.noreply.github.com>
2026-07-08 12:11:15 -0600

    Add restartBoard function to gameboard

--- script.js
+++ script.js
@@ -6,31 +6,40 @@ const createPlayers = (player1Name = "player1", player2Name = "player2") => {
   return {
     player1: {
       name: player1Name,
-      mark: "x",
+      mark: "X",
     },
     player2: {
       name: player2Name,
-      mark: "o",
+      mark: "0",
     }
   };
 };
 
 const gameboard = (() => {
   const board = [
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0]
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""]
   ];
 
   const getBoard = () => board;
-  
-  const addPlayerMark = (player, row, column) => {
-    board[row][column] = player.mark;
-  }
 
-  return {getBoard, addPlayerMark};
+  const restartBoard = () => {
+    for (let i = 0; i <= 2; i++) {
+      for (let j = 0; j <= 2; j++) {
+        board[i][j] = "";
+      }
+    }
+  };
+
+  return {getBoard, restartBoard};
 })();
 
 const gameflow = (() => {
   console.table(gameboard.getBoard());
+
+  //player 1 turn
+  //switch to next
+  //player 2 turn
+  //loop that until winning condition
 })();
