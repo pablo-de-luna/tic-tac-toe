d7161db658603a73559949406bdf92ed42427de7
Pablo De Luna <205702351+pablo-de-luna@users.noreply.github.com>
2026-07-13 12:29:41 -0600

    Refactor createPlayers function as a factory

--- script.js
+++ script.js
@@ -2,19 +2,18 @@
 
 "use strict"
 
-// MOVE THIS FUNCTION INSIDE A FACTORY AS A METHOD
-const createPlayers = (player1Name = "player1", player2Name = "player2") => {
-  return {
-    player1: {
-      name: player1Name,
-      mark: "X",
-    },
-    player2: {
-      name: player2Name,
-      mark: "0",
-    }
+const players = (() => {
+  const player1 = {name: "Player 1", mark: "X"};
+  const player2 = {name: "Player 2", mark: "0"};
+
+  const setPlayersName = (player1Name = "Player 1", player2Name = "Player 2") => {
+    player1.name = player1Name;
+    player2.name = player2Name;
   };
-};
+  const getPlayers = () => ({player1, player2});
+
+  return {setPlayersName, getPlayers}
+})();
 
 const gameboard = (() => {
   const board = [
@@ -24,25 +23,21 @@ const gameboard = (() => {
   ];
 
   const getBoard = () => board;
-
-  // MAYBE MOVE THIS METHOD TO ANOTHER FACTORY
-  const addPlayerMark = (player, row, column) => {
-    board[row][column] = player.mark;
-  };
-
+  const addPlayerMark = (player, row, column) => {board[row][column] = player.mark};
   const restartBoard = () => board.forEach(row => row.fill(""));
   
   return {getBoard, addPlayerMark, restartBoard};
 })();
 
-// RETHINK RENAMING OR SPLITING THIS FACTORY, PUT EACH METHOD IN A LOGICAL PLACE
 const gameControl = (() => {
-  // MAYBE CHANGE PLAYERS DECLARATION TO GAMEBOARD OR MAKE IT ACCESIBLE AS A METHOD
-  const {player1, player2} = createPlayers("Pablo", "Dutch");
   const board = gameboard.getBoard();
+  const {player1, player2} = players.getPlayers();
   let currentPlayer = player1;
 
-  //MAYBE THIS METHODS SHOULD BE IN GAMEBOARD
+  players.setPlayersName("Pablo", "Dutch")
+  console.table(players.getPlayers())
+  console.table({player1, player2})
+
   const getPlayer1 = () => player1;
   const getPlayer2 = () => player2;
   const getCurrentPlayer = () => currentPlayer;
@@ -51,7 +46,6 @@ const gameControl = (() => {
     const checkForRowWin = () => {
       return board.some(row => row.every(space => (space === row[0] && space !== "")));
     };
-
     const checkForColumnWin = () => {
       const columns = [0, 1, 2];
       // This check if SOME column have EVERY row space marked with equal player mark
@@ -59,7 +53,6 @@ const gameControl = (() => {
         return board.every(row => (row[col] === board[0][col] && board[0][col] !== ""))
       });
     };
-
     const checkForDiagonalWin = () => {
       return (board[1][1] !== "" && (
         (board[0][0] === board[1][1] && board[2][2] === board[1][1]) ||
@@ -89,15 +82,3 @@ const gameControl = (() => {
     checkIfSpaceIsTaken
   };
 })();
-
-//POSSIBLE WINNING COMBINATIONS
-// 111,000,000 
-// 000,111,000 HORIZONTAL 
-// 000,000,111 
-
-// 100,100,100 
-// 010,010,010 VERTICAL
-// 001,001,001 
-
-// 100,010,001 DIAGONAL
-// 001,010,100 
